a587c26ea71ed4b7b5188721ba8965f777c894f7
Mohamed Ali Ghouma <MAGhouma@se.linedata.com>
2020-06-28 18:54:34 +0100

    first commit

---
+++ src/main/java/demo/Mohmaed.java
@@ -0,0 +1,48 @@
+package demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Mohmaed
+ */
+@WebServlet("/hello")
+public class Mohmaed extends HttpServlet
+{
+   private static final long serialVersionUID = 1L;
+
+   /**
+    * @see HttpServlet#HttpServlet()
+    */
+   public Mohmaed()
+   {
+      super();
+      // TODO Auto-generated constructor stub
+   }
+
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+    *      response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+   {
+      // TODO Auto-generated method stub
+      response.getWriter().append("Served at: ").append(request.getContextPath());
+   }
+
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+    *      response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+   {
+      // TODO Auto-generated method stub
+      doGet(request, response);
+   }
+
+}
